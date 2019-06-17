cb567ac47d1d08911ac21fa31e15ccc1035cf1bd
Jayke Peters <jaykepeters@gmail.com>
2019-06-16 22:54:56 -0500

    Add files via upload

--- AppBlocker/AppBlocker/main.swift
+++ AppBlocker/AppBlocker/main.swift
@@ -15,17 +15,37 @@ let nc = NSWorkspace.shared.notificationCenter
 // Observer class
 class Observer {
     init() {
-        print("Initializing Observer")
         nc.addObserver(self, selector: #selector(notificationDidTrigger), name: NSWorkspace.willLaunchApplicationNotification, object: nil)
     }
+  
+    // Blacklisted Process Names
+    let blackListedProcessNames = ["com.apple.AppStore", "com.apple.Terminal"]
     
+    // Kill Process Method
+    private func killProcess(_ processId: Int) {
+        if let process = NSRunningApplication.init(processIdentifier: pid_t(processId)) {
+            print("Killing \(processId): \(String(describing: process.localizedName!))")
+            process.forceTerminate()
+        }
+    }
+    
+    // Application Launch Method
     @objc func notificationDidTrigger(notification: Notification) {
-        print("Notification triggered ", notification.userInfo)
+        // NOTIFICATION USER INFORMATION
+        let userInfo = notification.userInfo
+        
+        if let processBundleIdentifier: String = notification.userInfo?["NSApplicationBundleIdentifier"] as? String {
+            if let processId = userInfo?["NSApplicationProcessIdentifier"] as? Int {
+                if (blackListedProcessNames.contains(processBundleIdentifier)) {
+                    killProcess(processId)
+                }
+            }
+        }
     }
 }
 
 // Create a new observer
 let obj = Observer()
 
-// Keep the app running
+// Keep the tool running
 CFRunLoopRun()
