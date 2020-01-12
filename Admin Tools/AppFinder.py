8a2c49e98f23581c97cbc266c73e0291878861ed
Jayke Peters <jaykepeters@gmail.com>
2020-01-12 00:03:38 -0600

    Update AppFinder.py

--- Admin Tools/AppFinder.py
+++ Admin Tools/AppFinder.py
@@ -14,16 +14,35 @@ appsWithErrors = {
 }
 appDBJSON = "appdb.json"
 
+## FUNCTIONS
+def getBinaryPlistKey(plist, key):
+    out = subprocess.Popen(['defaults', 'read', plist, key], 
+        stdout=subprocess.PIPE, 
+        stderr=subprocess.STDOUT)
+    stdout, stderr = out.communicate()
+    return stdout.strip(), stderr
+
+def writeDict(_dict, file):
+    with open(file, "w") as outfile:
+        json.dump(_dict, outfile, indent=4, sort_keys=True)
+## END OF FUNCTIONS
+
 # Get all apps on this computer
 command="mdfind kind:application"
 apps = subprocess.check_output(command, shell=True).strip().split("\n")
+# apps = [
+#     "/Applications/Safari.app",
+#     "/Applications/iMovie.app",
+#     "/Applications/FaceTime.app"
+#] error with /Applications/Utilities/Adobe Flash Player Install Manager.app/Contents/Info.plist? # first binary plist too...
 
 # Additional plist keys to check for
 addtionalKeys = {
     "CFBundleDisplayName": "appName",
     "CFBundleName": "someName",
     "CFBundleExecutable": "executableName",
-    "CFBundleShortVersionString": "appVersion"
+    "CFBundleShortVersionString": "appVersion",
+    "LSApplicationCategoryType": "category"
 }
 
 # Iterate over every application, checking for a CFBundleIdentifier and more!
@@ -38,28 +57,35 @@ for app in apps:
         appsWithErrors['binaryPlist'].append(app)
     except IOError:
         appsWithErrors['noPlist'].append(app)
-    finally:
-        appsWithErrors['unknown'].append(app)
     
     # Assuming we got a valid, readable plist
     # Get the CFBundleIdentifier, or not
-    try:
+    try: 
         bundleID = _plist['CFBundleIdentifier']
     except:
         appsWithErrors['noCFBundleIdentifier'].append(app)
 
     # Ok, so we got the bundle id, let's get some more info 
     applicationDB[bundleID] = {}
+    applicationDB[bundleID]['appPath'] = app
     for key in addtionalKeys.keys():
         if key in _plist.keys():
             value = _plist[key]
             applicationDB[bundleID][addtionalKeys[key]] = value
-            
-# Output our findings to a file
-with open(appDBJSON, "w") as applicationDatabase:
-    json.dump(applicationDB, applicationDatabase, indent=4, sort_keys=True)
+
+# Try to get the binary plist keys (WORKAROUND) SEE DOCS
+for app in appsWithErrors['noCFBundleIdentifier']:
+    plist = '/'.join([app, 'Contents/Info'])
+    cmdOut = getBinaryPlistKey(plist, 'CFBundleIdentifier')
+    if not 'not' in cmdOut[0]:
+        print(cmdOut[0])
+
+# Output our findings to 2 files
+writeDict(applicationDB, appDBJSON)
+writeDict(appsWithErrors, 'appfinder_errors.json')
 
 # Let the user know some things :)
 print("Total Apps Found: "+str(len(apps)))
 print("Total Bundle Identifiers found: "+str(len(applicationDB)))
 print("The application database has been exported to: "+appDBJSON)
+print("Any apps with errors can be found in appErrors.json")
