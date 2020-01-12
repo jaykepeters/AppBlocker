be71cfe71ba60a2c8a169be16aacce6d2ff6e113
Jayke Peters <jaykepeters@gmail.com>
2020-01-11 21:02:18 -0600

    Update AppFinder.py

--- Admin Tools/AppFinder.py
+++ Admin Tools/AppFinder.py
@@ -2,29 +2,59 @@
 import subprocess # for running the commmand
 import plistlib # for getting bundle identifiers
 import json # for formatting the dict
+import xml.parsers.expat # for error checking
 
 # Initialize Necessary Types
 applicationDB = {}
-appsWithNoBundleID = []
+appsWithErrors = {
+    "noCFBundleIdentifier": [],
+    "binaryPlist": [],
+    "noPlist": [],
+    "unknown": []
+}
 appDBJSON = "appdb.json"
 
 # Get all apps on this computer
 command="mdfind kind:application"
 apps = subprocess.check_output(command, shell=True).strip().split("\n")
 
+# Additional plist keys to check for
+addtionalKeys = {
+    "CFBundleDisplayName": "appName",
+    "CFBundleName": "someName",
+    "CFBundleExecutable": "executableName",
+    "CFBundleShortVersionString": "appVersion"
+}
+
 # Iterate over every application, checking for a CFBundleIdentifier and more!
 for app in apps:
     # Set the plist path
     plist = '/'.join([app, 'Contents/Info.plist'])
-    
-    try:
-        bundleID = plistlib.readPlist(plist)['CFBundleIdentifier']
-        # Add to applicationsDB
-        applicationDB[bundleID] = {'applicationPath': app}
-    except:
-        # Add to appsWithNoBundleID
-        appsWithNoBundleID.append(app)
 
+    # Try to get the plist and/or bundle identifier
+    try:    
+        _plist = plistlib.readPlist(plist)
+    except xml.parsers.expat.ExpatError:
+        appsWithErrors['binaryPlist'].append(app)
+    except IOError:
+        appsWithErrors['noPlist'].append(app)
+    finally:
+        appsWithErrors['unknown'].append(app)
+    
+    # Assuming we got a valid, readable plist
+    # Get the CFBundleIdentifier, or not
+    try:
+        bundleID = _plist['CFBundleIdentifier']
+    except:
+        appsWithErrors['noCFBundleIdentifier'].append(app)
+
+    # Ok, so we got the bundle id, let's get some more info 
+    applicationDB[bundleID] = {}
+    for key in addtionalKeys.keys():
+        if key in _plist.keys():
+            value = _plist[key]
+            applicationDB[bundleID][addtionalKeys[key]] = value
+            
 # Output our findings to a file
 with open(appDBJSON, "w") as applicationDatabase:
     json.dump(applicationDB, applicationDatabase, indent=4, sort_keys=True)
