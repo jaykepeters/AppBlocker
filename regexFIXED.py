561b27d39f96927a8dd0c99e983c4788b5718449
Jayke Peters <jaykepeters@gmail.com>
2020-01-17 17:37:08 -0600

    see previous version if not working

--- regexFIXED.py
+++ regexFIXED.py
@@ -1,40 +1,89 @@
 #!/usr/bin/python
 from AppKit import NSWorkspace
+from Foundation import NSProcessInfo
 import os
 import signal 
+import re
 
-#blockedBundleIdentifiersCombined = "(" + ")|(".join(blockedBundleIdentifiers.keys()) + ")"
-violationQueue = {}
-regex = r"(com.microsoft.Word)|(com.apple.*)|(com.safeincloud.Safe-In-Cloud.OSX)"
+records = {
+    "com.apple.systempreferences": {
+        "alertMessage": "hello"
+    }
+}
 
-def getBlockedApps():
-    pass
+def parseConfig(config):
+    availableOptionKeys = {
+        "allowedGroups": list,
+        "allowedUsers": list,
+        "customAlert": {
+            "message": str,
+            "informativeText": str,
+            "iconPath": str
+            "proceedButton": str
+        },
+        "deleteBlockedApplication": bool
+    }
+
+    # Every blocked app
+    keyErrors = []
+    for blockedApp in config:
+        # Every key in blocked app entry
+        for key in blockedApp.keys():
+            # Is key type right?
+            if key in availableOptionKeys.keys() and type(key) is availableOptionKeys[key] and type(key) is not dict:
+                pass
+            elif type(key) is dict and key is "customAlert":
+                # for key in custom alert
+                for _key in key.keys():
+                    if _key in availableOptionKeys.keys():
+                        pass
+                    else:
+                        return # FAIL
+            else:
+                return # FAIL
+                    
+        
+    # for key in options.keys():
+    #     if key in availableOptionKeys.keys() and type(key) is availableOptionKeys[key]:
+    #         pass
+    #     else:
+    #         return False
+
+regex = r"(com.jamfsoftware.*)"
+
+violations = []
+
+def takeAction(_violationInfo):    
+    # Add the app to the queue
+    violations.append(_violationInfo)
+
+    # Kill the process
+    os.kill(_violationInfo['processIdentifier'], signal.SIGKILL)
+
+    # Extract the options
+    options = records[_violationInfo['matchedRegex']]
 
 def killRunningApps():
-    ##
     workspace = NSWorkspace.sharedWorkspace()
-    ##
-
     running_apps = workspace.runningApplications()
     for app in running_apps:
-        import re
-        matchedq = re.search(regex, str(app.bundleIdentifier()))
-        if matchedq:
-            _index = matchedq.lastindex - 1### WORKING
-            print re.sub(r"\(|\)", '', regex).split("|")[_index]
+        match = re.match(regex, str(app.bundleIdentifier()))
+        if match:
+            _index = match.lastindex - 1
+            exactBundleIDOrWildcard = re.sub(r"\(|\)", '', regex).split("|")[_index]
             
-        # We call the takeaction method, with the additions below
+            # We call the takeaction method, with the additions below
             violationInfo = {
-                "name": app.localizedName(),
+                "matchedRegex": exactBundleIDOrWildcard,
+                "userName": NSProcessInfo.processInfo().userName(),
+                "appName": app.localizedName(),
                 "bundleIdentifier": app.bundleIdentifier(),
-                "processIdentifier": app.processIdentifier()
+                "processIdentifier": app.processIdentifier(),
+                "appPath...": 0
             }
-            print(violationInfo)
-            #NSRUNNINGAPPLICATION
-           # violationQueue.append(violationInfo)
-            # Kill the PID of the app!
-            #os.kill(app.processIdentifier(), signal.SIGKILL)
-            #app.hide() soft block
+           
+            # Take action upon the app
+            takeAction(violationInfo)
 
 ## BETA TESTING
 if __name__ == "__main__":
