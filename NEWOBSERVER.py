d3664222c7c4861e4ab85424a8bfa1b65180e055
Jayke Peters <jaykepeters@gmail.com>
2020-01-19 15:46:35 -0600

    v2 see older ones for more info

--- NEWOBSERVER.py
+++ NEWOBSERVER.py
@@ -3,14 +3,31 @@
 import objc
 from Foundation import NSObject, NSKeyValueObservingOptionNew, NSKeyValueChangeNewKey
 
-import Foundation
-from AppKit import NSWorkspace
+"""
+Getting help from: 
+https://nshipster.com/key-value-observing/
+https://svn.red-bean.com/pyobjc/branches/pyobjc-1.4-branch/Examples/00ReadMe.html
+https://stackoverflow.com/questions/8317338/how-to-use-kvo-to-detect-when-an-application-gets-active
+https://stackoverflow.com/questions/52710089/macos-detect-all-application-launches-including-background-apps
 
+"""
+
+import Foundation
+import AppKit
 from PyObjCTools import AppHelper
 
+def takeaction(runningapp):
+    print(runningapp.bundleIdentifier())
+    
 class Observer(NSObject):
     def observeValueForKeyPath_ofObject_change_context_(self, path, object, changeDescription, context):
-        print 'path "%s" was changed to "%s".' % (path, changeDescription[NSKeyValueChangeNewKey])
+        if NSKeyValueChangeNewKey == 'new':
+            takeaction(object.runningApplications()[-1])
+        #print changeDescription[NSKeyValueChangeNewKey]
+        
+    def willChangeValueForKey_(self, key):
+        print "Changed"
+        
 
 observer = Observer.new()
 
@@ -20,4 +37,4 @@ workspace = Foundation.NSWorkspace.sharedWorkspace() # For runnning apps
 
 workspace.addObserver_forKeyPath_options_context_(observer, 'runningApplications', NSKeyValueObservingOptionNew, 0)
 
-AppHelper.runConsoleEventLoop()
+AppHelper.runConsoleEventLoop().run()
